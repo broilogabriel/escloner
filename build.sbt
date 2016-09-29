bfdf9c1ddf411dbf2596d7cf83088f8cdfadc4f5
Gabriel Broilo <gabriel.broilo@newswhip.com>
2016-09-29 17:10:59 +0100

    Connect to ES cluster and perform query

--- build.sbt
+++ build.sbt
@@ -7,3 +7,7 @@ scalaVersion := "2.11.8"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.0" % "test"
 
 libraryDependencies += "org.mockito" % "mockito-core" % "1.10.19" % "test"
+
+libraryDependencies += "org.elasticsearch" % "elasticsearch" % "1.7.5"
+
+mainClass in (Compile, run) := Some("com.broilogabriel.Main")
---
+++ src/main/scala/com/broilogabriel/Main.scala
@@ -0,0 +1,30 @@
+package com.broilogabriel
+
+import org.elasticsearch.action.search.SearchType
+import org.elasticsearch.client.transport.TransportClient
+import org.elasticsearch.common.settings.ImmutableSettings
+import org.elasticsearch.common.transport.InetSocketTransportAddress
+import org.elasticsearch.index.query.QueryBuilders
+
+/**
+  * Created by broilogabriel on 29/09/16.
+  */
+object Main extends App {
+
+  val client = Cluster.getCluster("remotecluster", "localhost", 9301)
+  val response = client.prepareSearch().setSearchType(SearchType.QUERY_AND_FETCH).
+    setQuery(QueryBuilders.matchAllQuery()).execute().actionGet()
+  println(response.toString)
+}
+
+object Cluster {
+
+  def getCluster(clusterName: String, address: String, port: Int) = {
+    val inetAddress = new InetSocketTransportAddress(address, port)
+    val settings = ImmutableSettings.settingsBuilder().put("cluster.name", clusterName).build()
+    new TransportClient(settings).addTransportAddress(inetAddress)
+  }
+
+}
+
+
